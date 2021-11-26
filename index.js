ad4e35052e0d517ab18763840699a936c2f169a1
richa88 <91276574+richa88@users.noreply.github.com>
2021-11-26 20:34:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+var person1={
+    FirstName: "Riya",
+    LastName: "Mishra",
+    OfficeAddress: "3rd Floor,Lakshmi Complex,Rajendra Nagar,Chennai"
+  }
+
+console.log("First Person:");
+console.log("Name-"+person1.FirstName+" "+person1.LastName);
+console.log("Office Address-"+person1.OfficeAddress);
+
+var person2={
+    FirstName: "Shreya",
+    LastName: "Singh",
+    OfficeAddress: "1st Floor,Jeevan Deep,Exhibition Road,Patna"
+}
+
+console.log("Second Person:");
+console.log("Name-"+person2.FirstName+" "+person2.LastName);
+console.log("Office Address-"+person2.OfficeAddress);
+
+var person3={
+  FirstName: "Supriya",
+  LastName: "Chopra",
+  OfficeAddress: "2nd Floor,Jeevan Prakash,Rajpath Nagar,Delhi"
+}
+
+console.log("Third Person:");
+console.log("Name-"+person3.FirstName+" "+person3.LastName);
+console.log("Office Address-"+person3.OfficeAddress);
+
